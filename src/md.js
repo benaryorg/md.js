90f35a0159985ed67eee64814bcf39a36e3e6cc4
benaryorg <binary@benary.org>
2016-05-06 14:20:39 +0200

    fix email in copyright

--- src/md.js
+++ src/md.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 benaryorg (benaryorg@keybase.io)
+ * Copyright (c) 2015 benaryorg (binary@benary.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the “Software”), to deal
